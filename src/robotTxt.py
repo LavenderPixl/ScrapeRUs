73f1923cde8316125773ebfa3a2c53c3c5dd37b3
LavenderPixl <annajacobsen1999@hotmail.com>
2024-11-25 08:55:00 +0100

    Can now read a URL's robot txt and scan allowed pages, for agents/spiders.
    Still need to check for agents NOT in list.

--- src/robotTxt.py
+++ src/robotTxt.py
@@ -4,25 +4,22 @@ from bs4 import BeautifulSoup
 baseUrl = 'https://en.wikipedia.org'
 
 
-def crawl(agent):
-    allowed, disallowed = sort_allowed(agent)
-    print(f"Allowed: {allowed}, Disallowed: {disallowed}")
+my_agents = ['*', 'LavSpidey']
 
 
-def sort_allowed(current_agent):
-    robots_txt = set_agent(current_agent, baseUrl)
-    if robots_txt == "err":
+def setup_robot_txt():
+    txt = get_robot_txt(baseUrl)
+    parsed_text = read_robot_txt(txt)
+    if parsed_text == "err":
         print("Error - Could not get robots txt.")
         exit(1)
 
-    allowed, disallowed = robots_txt[current_agent]
-    return allowed, disallowed
+    for agent in my_agents:
+        allowed, disallowed = parsed_text[agent]
+        print(f'Allowed: {allowed}, Disallowed: {disallowed}')
 
 
-def set_agent(current_agent, base_url):
-    robot_txt = get_robot_txt(base_url)
-    agents = read_robot_txt(current_agent, robot_txt)
-    return agents
+    # sort_allowed(parsed_text)
 
 
 def get_robot_txt(base_url):
@@ -30,10 +27,11 @@ def get_robot_txt(base_url):
     if result.status_code != 200:
         print(f"Error - Status code: {result.status_code}")
         exit(1)
-    return str(result.text)
+    return result.text
 
 
-def read_robot_txt(current_agent, robot_txt):
+def read_robot_txt(robot_txt):
+    current_agent = ''
     agents = {}
     for line in robot_txt.splitlines():
         # Skip empty lines and comments.
@@ -54,3 +52,18 @@ def read_robot_txt(current_agent, robot_txt):
 
         agents[current_agent] = (allowed, disallowed)
     return agents
+
+# def sort_allowed(parsed_text):
+#     robots_txt = set_agent(parsed_text, baseUrl)
+#
+#     allowed, disallowed = robots_txt[parsed_text]
+#     return allowed, disallowed
+
+# def set_agent(current_agent, base_url):
+#     robot_txt = get_robot_txt(base_url)
+#     agents = read_robot_txt(current_agent, robot_txt)
+#     return agents
+
+def crawl(agent):
+    allowed, disallowed = sort_allowed(agent)
+    print(f"Allowed: {allowed}, Disallowed: {disallowed}")
