5c9db898ee0b760dd11d950fcdbc7da7792dc973
LavenderPixl <annajacobsen1999@hotmail.com>
2025-01-10 10:24:34 +0100

    More Pandas optimization.
    Handling Empty file and incorrect field/file instantiation.

--- src/csvHandling.py
+++ src/csvHandling.py
@@ -2,6 +2,8 @@ import csv
 import pandas as pd
 import os.path
 
+from pandas.errors import EmptyDataError
+
 file_name = "VisitedSites.csv"
 
 fields = ['Url', 'Domain', 'Allowed', 'Disallowed']
@@ -11,7 +13,7 @@ rows = []
 def csv_start_up():
     if not os.path.isfile(file_name):
         with open(file_name, 'w') as csv_file:
-            writer = csv.writer(csv_file)
+            writer = csv.writer(csv_file, delimiter=' ')
             writer.writerow(fields)
     return file_name
 
@@ -29,20 +31,34 @@ def add_to_csv(file, domains):
 
         i = [domain.url, domain.domain, domain.allowed, domain.disallowed]
         with open(file, 'a') as csv_file:
-            writer = csv.writer(csv_file)
+            writer = csv.writer(csv_file, delimiter=' ')
             writer.writerow(i)
         rows_added += 1
 
     print(f"Rows added: {rows_added}")
 
+
+# def edit_row():
+
+
 def check_if_exists(file, domain):
-    data_frame = pd.read_csv(file)
-    if domain.url in data_frame['Url'].unique():
-        return True
-    return False
+    try:
+        data_frame = pd.read_csv(file)
+        if data_frame is None or data_frame.empty:
+            return False
+
+        if domain.url in data_frame['Url'].unique():
+            return True
+        return False
+    # If file is Empty
+    except EmptyDataError:
+        csv_start_up()
+    # If Field does not exist (File incorrectly created.)
+    except KeyError:
+        os.remove(file)
+        csv_start_up()
+
 
 def print_csv_data():
-    with open(file_name, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            print(row)
+    data_frame = pd.read_csv(file_name)
+    print(data_frame)
